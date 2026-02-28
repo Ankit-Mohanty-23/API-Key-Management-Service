8e2d81a6f6e09869c39e3744bf8a84f856b6ca1d
Ankit Mohanty <ankitmsb760@gmail.com>
2026-03-01 00:24:44 +0530

    feat(app): integrate globalError and notFound middlewares and add health check route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import logger from "./utils/logger.js";
-//import globalErrorHandler from "../src/middleware/error.middleware.js";
-//import { notFound } from "../src/middleware/notFound.middleware.js";
+import globalErrorHandler from "../src/middlewares/error.middleware.js";
+import { notFound } from "../src/middlewares/notFound.middleware.js";
 
 const app = express();
 
@@ -11,24 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(
-    morgan("dev", {
-        stream: {
-            write: (message) => logger.info(message.trim()),
-        },
-    })
+  morgan("dev", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
 );
 
 //Routes
-app.get("/ready", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "SmartQ API running",
-    });
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "SmartQ API running",
+  });
 });
 
 // user routes
 
-//app.use(notFound());
-//app.use(globalErrorHandler());
+app.use(notFound);
+app.use(globalErrorHandler);
 
 export default app;
